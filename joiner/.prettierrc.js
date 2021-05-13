a7bc7f472b48462a4d616cd3faf100978f6426c1
Gimoring <62681451+Gimoring@users.noreply.github.com>
2021-05-13 13:04:33 +0900

    기본설정

---
+++ joiner/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2, //들여쓰기 2칸
+  useTabs: false, // Tab 대신에 스페이스 사용!
+  semi: true, // 코드 마지막에 세미콜론을 추가해줍니다
+  singleQuote: true, // " 이거 말고 ' 이거로 되게.
+  trailingComma: 'all', // 객체,배열 뒤에 항상 콤마 추가해줍니다.
+  printWidth: 80, // 코드 한 줄의 최대 길이는 80입니다.
+  arrowParens: 'avoid', //Arrow 함수에서 매개변수 하나일 때 괄호 생략해줍니다.
+};
