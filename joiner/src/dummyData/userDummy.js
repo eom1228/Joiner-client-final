f650f60d9d12b0c3dbe6da4931a3de2618d266a3
Gimoring <62681451+Gimoring@users.noreply.github.com>
2021-05-18 16:35:50 +0900

    Dummy#None - 유저,그룹 더미데이터

---
+++ joiner/src/dummyData/userDummy.js
@@ -0,0 +1,29 @@
+const data = {
+  users: [
+    {
+      id: 1,
+      userName: 'taehoPouding',
+      email: 'taeho@github.com',
+      location: '부싼',
+      group: '애완동물산책모임',
+      event: '',
+    },
+    {
+      id: 2,
+      userName: 'minjiTheCarBreaker',
+      email: 'minji@github.com',
+      location: '마계인천',
+      group: '자동차부수기모임',
+      event: '',
+    },
+    {
+      id: 3,
+      userName: 'minsuAddictedCaffeine',
+      email: 'minsu@github.com',
+      location: '마계인천',
+      group: '커피에죽고사는불꽃남자들의모임',
+      event: '',
+    },
+  ],
+};
+export default data;
