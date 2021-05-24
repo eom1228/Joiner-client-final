0d0ebdcf7bed4ebdececedcbf6fa5c6391af0584
Gimoring <62681451+Gimoring@users.noreply.github.com>
2021-05-24 22:38:54 +0900

    Add#43 - 스크롤이벤트 추가

--- joiner/src/App.js
+++ joiner/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useReducer, useEffect } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import { UserProvider, useUserState, useUserDispatch } from './UserContext';
@@ -12,96 +11,18 @@ import Footer from './components/footer';
 import MyPage from './components/myPage';
 import GroupPage from './components/groupPage';
 
-import Test from './test';
 import './App.css';
 
 const App = () => {
-  // const state = useUserState();
-  // const dispatch = useUserDispatch();
-
-  //   const GlobalStyle = createGlobalStyle`
-  // 		body {
-
-  // 		}
-  // 	`;
-  //   const state = useContext(UserStateContext);
-  //   const dispatch = useContext(UserDispatchContext);
-
-  //   console.log(state);
-
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-
-    <UserProvider>
-      <Test />
-
-      <Route
-        exact
-        path="/" // landing이 홈
-        render={() => (
-          <div>
-            <NavBar />
-            <LandingPage />
-            <Footer />
-          </div>
-        )}
-      />
-      <Switch>
-        <Route path="/login" render={() => <Login />} />
-        <Route exact path="/signUp" render={() => <Signup />} />
-        <Route
-          exact
-          path="/main"
-          render={() => (
-            <div>
-              <NavBar />
-              <MainPage user={user} />
-              <Footer />
-            </div>
-          )}
-        />
-        <Route
-          exact
-          path="/mypage"
-          render={() => (
-            <div>
-              <NavBar />
-              <MyPage user={user} location={location}>
-                <User />
-              </MyPage>
-              <Footer />
-            </div>
-          )}
-        />
-        {/* <Route 
-					exact path='/GroupsByCategoryPage' 
-					render={() => 
-						(<div>
-							<NavBar />
-							<GroupsByCategory />
-							<Footer />
-						</div>)
-					} 
-				/> */}
-        <Route
-          exact
-          path="/GroupPage"
-          render={() => (
-            <div>
-              <NavBar />
-              <GroupPage />
-              <Footer />
-            </div>
-          )}
-        />
-      </Switch>
-    </UserProvider>
-
+    <>
+      <LandingPage />
+    </>
   );
 };
 
-export default withRouter(App);
+export default App;
+// export default withRouter(App);
 
 // function App() {
 //   const [isOpen, setIsOpen] = useState(false);
@@ -120,5 +41,71 @@ export default withRouter(App);
 //   );
 // }
 
-
 // export default App;
+
+// ================
+// {/* <UserProvider>
+//       <Test />
+
+//       <Route
+//         exact
+//         path="/" // landing이 홈
+//         render={() => (
+//           <div>
+//             <NavBar />
+//             <LandingPage />
+//             <Footer />
+//           </div>
+//         )}
+//       />
+//       <Switch>
+//         <Route path="/login" render={() => <Login />} />
+//         <Route exact path="/signUp" render={() => <Signup />} />
+//         <Route
+//           exact
+//           path="/main"
+//           render={() => (
+//             <div>
+//               <NavBar />
+//               <MainPage user={user} />
+//               <Footer />
+//             </div>
+//           )}
+//         />
+//         <Route
+//           exact
+//           path="/mypage"
+//           render={() => (
+//             <div>
+//               <NavBar />
+//               <MyPage user={user} location={location}>
+//                 <User />
+//               </MyPage>
+//               <Footer />
+//             </div>
+//           )}
+//         />
+//         {/* <Route
+// 					exact path='/GroupsByCategoryPage'
+// 					render={() =>
+// 						(<div>
+// 							<NavBar />
+// 							<GroupsByCategory />
+// 							<Footer />
+// 						</div>)
+// 					}
+// 				/> */}
+//         <Route
+//           exact
+//           path="/GroupPage"
+//           render={() => (
+//             <div>
+//               <NavBar />
+//               <GroupPage />
+//               <Footer />
+//             </div>
+//           )}
+//         />
+//       </Switch>
+//     </UserProvider>
+//    */}
