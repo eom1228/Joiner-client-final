48085751d24939d701fb093d0077eb9b2f62d034
Gimoring <62681451+Gimoring@users.noreply.github.com>
2021-05-27 11:39:26 +0900

    #Add#43 - LandingPage Layout

--- joiner/src/components/card.jsx
+++ joiner/src/components/card.jsx
@@ -4,6 +4,7 @@ const Card = props => {
   return (
     <>
       <div className="card">
+        {/* <h3>사진사진사진사진</h3> */}
         <img src={props.img} alt="card" className="card_img" />
       </div>
     </>
